34f5ca3142e10dc1de270731ff7fea7c5f1d3d86
Merkan A. <merkan.aksoydn@gmail.com>
2026-09-17 23:22:39 +0200

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -26,6 +26,8 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
     const iconPath = joinSegments(baseDir, "static/icon.png")
+    const siteBasePath = ctx.argv.serve || !cfg.baseUrl ? "" : url.pathname.replace(/\/$/, "")
+    const staticPath = (file: string) => `${siteBasePath}/static/${file}`
 
     // Url of current page
     const socialUrl =
@@ -64,13 +66,10 @@ export default (() => {
           href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,400;0,9..144,500;0,9..144,600;0,9..144,700;1,9..144,400&family=Inter:wght@400;500;600&family=JetBrains+Mono:wght@400;500;600&display=swap"
         />
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
-        <script defer src={joinSegments(baseDir, "static/insan-misin/overlay.js")}></script>
-        <script defer src={joinSegments(baseDir, "static/bura-corner.js")}></script>
-        <script defer src={joinSegments(baseDir, "static/story-ui.js")}></script>
-        <script
-          defer
-          src={`${joinSegments(baseDir, "static/loss-records.js")}?v=20260917-found`}
-        ></script>
+        <script defer src={staticPath("insan-misin/overlay.js")}></script>
+        <script defer src={`${staticPath("bura-corner.js")}?v=20260917-records`}></script>
+        <script defer src={staticPath("story-ui.js")}></script>
+        <script defer src={`${staticPath("loss-records.js")}?v=20260917-found`}></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
