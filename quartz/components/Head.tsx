5218ee7a6d70cbae748927b94b5c3f2bb4d34cee
Merkan A. <merkan.aksoydn@gmail.com>
2026-09-17 11:15:19 +0200

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -65,6 +65,7 @@ export default (() => {
         />
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <script defer src={joinSegments(baseDir, "static/insan-misin/overlay.js")}></script>
+        <script defer src={joinSegments(baseDir, "static/bura-corner.js")}></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
