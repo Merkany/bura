5f6f9e768ed9fc8379767e177024821d57d1f5d9
Merkan A. <merkan.aksoydn@gmail.com>
2026-09-19 12:26:49 +0200

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,14 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { componentRegistry } from "./quartz/components/registry"
+import { buraSocialImage } from "./quartz/util/buraSocialImage"
+
+componentRegistry.setOptionOverrides("@quartz-community/og-image", {
+  colorScheme: "lightMode",
+  width: 1200,
+  height: 630,
+  excludeRoot: false,
+  imageStructure: buraSocialImage,
+})
 
 const config = await loadQuartzConfig()
 export default config
